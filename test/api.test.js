1cd0b56f3c699b0e46be19c151a7a77acd3785a0
gamzamandu <galbimandu1126@gmail.com>
2024-10-30 18:36:56 +0900

    bugfix : test 코드가 계속 실행되는 오류 수정

--- test/api.test.js
+++ test/api.test.js
@@ -8,14 +8,20 @@ const { expect } = chai;
 
 
 describe("생성 API 테스트", () => {
-  it("GET /room should return a 201 status", (done) => {
+  it("POST /room should return a 201 status", (done) => {
     chai.request.execute(server)
       .post('/room')
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(201);
         expect(res.body).to.be.an('object');
+        expect(res.body.message).is.string;
+        expect(res.body.roomCode).is.string;
         done();
       });
   });
+
+  after(() => {
+    server.close;
+  })
 });
