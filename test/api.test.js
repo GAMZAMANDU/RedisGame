856a7cdb035b51ac243c7e4d5c6c1ac809b2ebab
gamzamandu <galbimandu1126@gmail.com>
2024-10-31 11:38:44 +0900

    add : 방 생성 후 방 조회 TDD

--- test/api.test.js
+++ test/api.test.js
@@ -2,7 +2,7 @@ import * as chaiModule from "chai";
 import chaiHttp from "chai-http";
 
 const chai = chaiModule.use(chaiHttp);
-import {server} from '../index.js';
+import { server } from "../index.js";
 
 const { expect } = chai;
 
@@ -10,18 +10,43 @@ const { expect } = chai;
 describe("생성 API 테스트", () => {
   it("POST /room should return a 201 status", (done) => {
     chai.request.execute(server)
-      .post('/room')
+      .post("/room")
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(201);
-        expect(res.body).to.be.an('object');
-        expect(res.body.message).is.string;
-        expect(res.body.roomCode).is.string;
+        expect(res.body).to.be.an("object");
+        expect(res.body.message).to.be.a("string");
+        expect(res.body.roomCode).to.be.a("string");
+
         done();
       });
   });
+});
 
-  after(() => {
-    server.close;
+describe("생성 후 방 조회 API 테스트", () => {
+  it("GET /room/:roomCode should return a 200 status", (done) => {
+    chai.request.execute(server)
+      .post("/room")
+      .end((err, res) => {
+        const { roomCode } = res.body;
+        const { targetNumber } = res.body;
+        
+        chai.request.execute(server)
+          .get(`/room/${roomCode}`)
+          .query({ tryNumber : targetNumber })
+          .end((err, res) => {
+            // console.log(res.body);
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an("object");
+            expect(res.body.status).to.equal("일치하지 않음");
+
+            done();
+          });
+      });
+
+  it("GET /room/:roomCde, tryNumber != targetNumber -> should return 일치했습니다", (done) => {
+    chai.request.execute(server)
+    .GET
   })
 });
